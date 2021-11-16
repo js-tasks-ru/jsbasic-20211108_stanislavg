f847fb8d6341b1d04fcde5c8503c52bf54e07262
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-11-16 20:25:51 +0300

    check spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowercaseStr = str.toLowerCase();
+  console.log(lowercaseStr);
+  if (lowercaseStr.includes('1xBet'.toLowerCase()) || (lowercaseStr.includes('XXX'.toLowerCase()))) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
