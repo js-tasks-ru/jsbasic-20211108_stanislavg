7daa94ad8813dfa9d4dff062ad9042ab111a317e
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-11-17 19:51:05 +0300

    fixes

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,10 +1,5 @@
 function checkSpam(str) {
   let lowercaseStr = str.toLowerCase();
-  console.log(lowercaseStr);
-  if (lowercaseStr.includes('1xBet'.toLowerCase()) || (lowercaseStr.includes('XXX'.toLowerCase()))) {
-    return true;
-  } else {
-    return false;
-  }
+  return lowercaseStr.includes('1xBet'.toLowerCase()) || (lowercaseStr.includes('XXX'.toLowerCase()));
 }
 
