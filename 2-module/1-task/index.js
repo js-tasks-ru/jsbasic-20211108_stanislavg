08d0ba11ff7c22c17942719f50c1ad2570edd032
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-11-22 14:26:33 +0300

    fixes

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,18 +3,10 @@ function sumSalary(salaries) {
 
   for (let salary of Object.values(salaries)) {
 
-    if (typeof salary === 'number' && !!isFinite(salary)) {
+    if (typeof salary === 'number' && isFinite(salary)) {
       sum += salary;
     }
   }
 
   return sum;
 }
-
-let salaries = {
-  John: 1000,
-  Ann: 1000,
-  Pete: 1000,
-};
-
-sumSalary(salaries);
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,7 +1,7 @@
 let calculator = {
   read(a, b) {
-    consolelog(this.a = +prompt("Введите значение 1", 0));
-    consolelog(this.b = +prompt("Введите значение 2", 0));
+    this.a = a;
+    this.b = b;
   },
 
   sum() {
@@ -12,10 +12,6 @@ let calculator = {
     return this.a * this.b;
   }
 };
-
-calculator.read();
-console.log(calculator.sum()); // 8
-console.log(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
