0d449ba33cf37d6c0b7bd8a5d5005564b6ab09fb
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-11-26 20:16:44 +0300

    arr tasks

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let splitedArr = str.split('-');
+
+  let newArr = splitedArr.map((elem, i) =>
+    i === 0 ? elem : elem[0].toUpperCase() + elem.slice(1))
+    .join('');
+
+  return newArr;
 }
+
+
