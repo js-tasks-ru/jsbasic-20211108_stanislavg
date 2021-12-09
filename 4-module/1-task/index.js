7105e852f63717aa0a28d29c959f470fa0dbc192
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-09 23:15:18 +0300

    tables

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let friendsList = document.createElement("ul");
+
+  let changedFriends = friends.map(friend => `${friend.firstName} ${friend.lastName}`);
+
+  changedFriends.forEach(element => {
+    let friendItem = document.createElement("li");
+    friendItem.textContent = element;
+
+    friendsList.appendChild(friendItem);
+  });
+
+  return friendsList;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let row of table.rows) {
+    for (let cell of row.cells) {
+      if (cell.dataset.available == "true") {
+        row.classList.add('available');
+      // } else if ()) {
+      //   row.setAttribute("hidden", true);
+      } else if (cell.dataset.available == "false") {
+        row.classList.add('unavailable');
+      }
+      if (cell.textContent == "m") {
+        row.classList.add('male');
+      }
+      if (cell.textContent == "f") {
+        row.classList.add('female');
+      }
+
+      if (cell.textContent <= 18) {
+        row.style.textDecoration = 'line-through';
+      }
+    }
+  }
 }
