4cfa7f6f2d86b8dff16826abe44177c1783d07ce
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-10 23:26:20 +0300

    fixes table

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,24 +1,57 @@
+// function highlight(table) {
+
+//   for (let row of table.rows) {
+//     for (let cell of row.cells) {
+//       if (cell.dataset.available === "true") {
+//         row.classList.add('available');
+//       // } else if ()) {
+//       //   row.setAttribute("hidden", true);
+//       } else if (cell.dataset.available === "false") {
+//         row.classList.add('unavailable');
+//       }
+//       if (cell.textContent === "m") {
+//         row.classList.add('male');
+//       }
+//       if (cell.textContent === "f") {
+//         row.classList.add('female');
+//       }
+
+//       if (cell.textContent <= 18) {
+//         row.style.textDecoration = 'line-through';
+//       }
+//     }
+//   }
+// }
+
 function highlight(table) {
 
-  for (let row of table.rows) {
-    for (let cell of row.cells) {
-      if (cell.dataset.available == "true") {
-        row.classList.add('available');
-      // } else if ()) {
-      //   row.setAttribute("hidden", true);
-      } else if (cell.dataset.available == "false") {
-        row.classList.add('unavailable');
-      }
-      if (cell.textContent == "m") {
-        row.classList.add('male');
-      }
-      if (cell.textContent == "f") {
-        row.classList.add('female');
-      }
+  for (i = 1; i < table.rows.length; i++) {
+    let age = table.rows[i].cells[1];
+    let gender = table.rows[i].cells[2];
+    let status = table.rows[i].cells[3];
 
-      if (cell.textContent <= 18) {
-        row.style.textDecoration = 'line-through';
-      }
+    if (age.textContent <= 18) {
+      age.parentElement.style.textDecoration = 'line-through';
+    }
+
+    if (gender.textContent === "m") {
+      age.parentElement.classList.add('male');
+    }
+
+    if (gender.textContent === "f") {
+      age.parentElement.classList.add('female');
+    }
+
+    if (status.dataset.available === "true") {
+      age.parentElement.classList.add('available');
+    }
+
+    if (status.dataset.available === "false") {
+      age.parentElement.classList.add('unavailable');
+    }
+
+    if (!status.dataset.available) {
+      status.parentElement.setAttribute("hidden", true);
     }
   }
 }
