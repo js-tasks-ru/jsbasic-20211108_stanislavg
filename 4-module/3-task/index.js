ef300cca385263c0ed1cbc6347c81068f3ba11a9
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-10 23:31:31 +0300

    fixes

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,28 +1,3 @@
-// function highlight(table) {
-
-//   for (let row of table.rows) {
-//     for (let cell of row.cells) {
-//       if (cell.dataset.available === "true") {
-//         row.classList.add('available');
-//       // } else if ()) {
-//       //   row.setAttribute("hidden", true);
-//       } else if (cell.dataset.available === "false") {
-//         row.classList.add('unavailable');
-//       }
-//       if (cell.textContent === "m") {
-//         row.classList.add('male');
-//       }
-//       if (cell.textContent === "f") {
-//         row.classList.add('female');
-//       }
-
-//       if (cell.textContent <= 18) {
-//         row.style.textDecoration = 'line-through';
-//       }
-//     }
-//   }
-// }
-
 function highlight(table) {
 
   for (i = 1; i < table.rows.length; i++) {
