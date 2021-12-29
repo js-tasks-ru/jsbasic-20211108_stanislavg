f04cd0e8fcf92ed61f28329edb0c81d6897bd0f7
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-29 21:51:38 +0300

    carousel

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,76 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = null; // DOM
+    this.templateTable = this.templateTable(); // table -string
+    this.templateToElements(this.templateTable); // table DOM
+    this.addHandler();
+
+  }
+
+  buttonHandler(e) {
+    if (e.target.nodeName === 'BUTTON') {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+
+  addHandler() {
+
+    this.elem.addEventListener('click', this.buttonHandler);
+  }
+
+  templateTable() {
+    return (
+      `<table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${this.createRows()}
+        </tbody>
+      </table>`
+    );
+  }
+
+  templateToElements(template) { // string -> DOM
+    const newElement = document.createElement('div');
+    newElement.innerHTML = template;
+
+    this.elem = newElement.firstChild;
+  }
+
+  createRows() {
+    return (
+      this.rows.map(el => this.templateRow(el)).join('')
+    );
+  }
+
+  templateRow(el) {
+    return (`
+      <tr>
+        <td>${el.name}</td>
+        <td>${el.age}</td>
+        <td>${el.salary}</td>
+        <td>${el.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    `);
+  }
+
+  handlerClickRemove(e) {
+    if (e.target === 'button') {
+      e.remove(); // del <tr>
+    }
+  }
+
+  handelEvent(tr) {
+    tr.addEventListener('click', handlerClickRemove);
   }
 }
